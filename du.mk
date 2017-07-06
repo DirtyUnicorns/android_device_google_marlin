09e57307449ee13d65b254cd386f7851fc4b0cf0
David Kataja Jr <krash86.7@gmail.com>
2017-07-06 17:52:23 -0400

    Marlin: Update build fingerprint to NZH54B
    Change-Id: I9f1c02a973368a4a2bd2214e63cf562fa2a62cf1

--- du.mk
+++ du.mk
@@ -32,5 +32,5 @@ PRODUCT_MANUFACTURER := Google
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    BUILD_FINGERPRINT=google/marlin/marlin:7.1.2/NKG47L/4025097:user/release-keys \
-    PRIVATE_BUILD_DESC="marlin-user 7.1.2 NKG47L 4025097 release-keys"
+    BUILD_FINGERPRINT=google/marlin/marlin:7.1.2/NZH54B/4045503:user/release-keys \
+    PRIVATE_BUILD_DESC="marlin-user 7.1.2 NZH54B 4045503 release-keys"
