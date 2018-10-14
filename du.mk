130ae764276c87ea9a9d122c212dfdad4baaef70
DeviousFusion <dj3tusk@gmail.com>
2018-10-13 22:18:13 -0500

    marlin: October 2018 PPR2.181005.003
    Change-Id: Ibd9d043b23d9f954d70a854d15ad028f111267b8

--- du.mk
+++ du.mk
@@ -30,7 +30,7 @@ $(call inherit-product, vendor/du/config/common_full_phone.mk)
 # Custom device configuration
 
 $(call inherit-product-if-exists, vendor/google/marlin/device-vendor-marlin.mk)
-$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk) #TODO: To be removed
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
 
 ## Camera
 PRODUCT_PACKAGES += \
@@ -49,14 +49,14 @@ PRODUCT_PACKAGES += \
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := du_marlin
 PRODUCT_DEVICE := marlin
-PRODUCT_BRAND := google
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel XL
 TARGET_MANUFACTURER := HTC
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    PRIVATE_BUILD_DESC="marlin-user 9 PPR2.180905.006 4945130 release-keys"
+    PRIVATE_BUILD_DESC="marlin-user 9 PPR2.181005.003 4984323 release-keys"
 
-BUILD_FINGERPRINT="google/marlin/marlin:9/PPR2.180905.006/4945130:user/release-keys"
-BUILD_THUMBPRINT="9/PPR2.180905.006/4945130:user/release-keys"
+BUILD_FINGERPRINT="google/marlin/marlin:9/PPR2.181005.003/4984323:user/release-keys"
+BUILD_THUMBPRINT="9/PPR2.181005.003/4984323:user/release-keys"
