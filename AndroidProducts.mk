cdf7626ad935f95f185d50edad2d65157198dad7
Josh Chasky <jchasky@gmail.com>
2019-10-03 08:52:18 -0400

    Clean up + add deps
    Change-Id: Icc215a63c84e6fe3e3f1174aa1a8c6cbd36e8629

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aosp_marlin.mk \
-	$(LOCAL_DIR)/du_marlin.mk
+	$(LOCAL_DIR)/du.mk
 
-COMMON_LUNCH_CHOICES := \
-	du_marlin-user \
-	du_marlin-userdebug
+#COMMON_LUNCH_CHOICES := \
+#	du_marlin-user \
+#	du_marlin-userdebug
